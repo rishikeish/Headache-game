24ace1fc1e5b1a00fffd0546a9ac37463e59f5b8
Rishikeish <krishricky4561@gmail.com>
2019-08-05 02:58:12 +0530

    changed everything

--- app/src/main/java/com/example/student/headache2/lvl3_Activity.java
+++ app/src/main/java/com/example/student/headache2/lvl3_Activity.java
@@ -1,7 +1,9 @@
 package com.example.student.headache2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class lvl3_Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class lvl3_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lvl3_);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this , level04_game.class);
+        startActivity(intent);
+
     }
 }
