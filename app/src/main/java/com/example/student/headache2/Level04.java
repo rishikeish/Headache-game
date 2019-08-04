60306ac0b7560fdaff8182fbdbc26e397f5a73be
Rishikeish <krishricky4561@gmail.com>
2019-08-04 23:17:14 +0530

    level 04 included

--- app/src/main/java/com/example/student/headache2/Level04.java
+++ app/src/main/java/com/example/student/headache2/Level04.java
@@ -1,7 +1,9 @@
 package com.example.student.headache2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Level04 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Level04 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level04);
+    }
+
+    protected void onClick(View view){
+
+        Intent intent = new Intent(this , level04_game.class);
+        startActivity(intent);
+
+
     }
 }
