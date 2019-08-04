03fc4c574c6e8966cce8cebe56df753532562afe
Rishikeish <krishricky4561@gmail.com>
2019-08-04 22:14:29 +0530

    level 04 included

--- app/src/main/java/com/example/student/headache2/Level04.java
+++ app/src/main/java/com/example/student/headache2/Level04.java
@@ -1,15 +1,13 @@
 package com.example.student.headache2;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Level04 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_level04);
     }
-
-
 }
