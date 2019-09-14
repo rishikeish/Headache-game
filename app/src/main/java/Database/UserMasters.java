b685d3f3a1d8a87f87f30886d3ca9451e184f9d0
hiarpoetry <hiar.soul@gmail.com>
2019-09-14 07:51:58 +0530

    backup update

---
+++ app/src/main/java/Database/UserMasters.java
@@ -0,0 +1,56 @@
+package Database;
+
+import android.provider.BaseColumns;
+
+public final class UserMasters {
+
+        private UserMasters(){}
+
+        protected static class Users implements BaseColumns{
+
+            public static final String TABLE_NAME = "USER_TABLE";
+            public static final String COL_1 = "USERNAME";
+            public static final String COL_2 = "PASSWORD";
+
+        }
+
+        protected static class Level1 implements BaseColumns{
+
+            public static final String TABLE_NAME = "LEVEL1_TABLE";
+            public static final String COL_1 = "ID";
+            public static final String COL_2 = "TERM";
+            public static final String COL_3 = "ANSWER";
+
+        }
+
+        protected static class Level2 implements BaseColumns{
+
+            public static final String TABLE_NAME = "LEVEL2_TABLE";
+            public static final String COL_1 = "ID";
+            public static final String COL_2 = "TERM";
+            public static final String COL_3 = "ANSWER";
+
+        }
+
+        protected static class Level3 implements BaseColumns{
+
+            public static final String TABLE_NAME = "LEVEL3_TABLE";
+            public static final String COL_1 = "ID";
+            public static final String COL_2 = "TERM";
+            public static final String COL_3 = "ANSWER";
+
+        }
+
+        protected static class Level4 implements BaseColumns{
+
+            public static final String TABLE_NAME = "LEVEL4_TABLE";
+            public static final String COL_1 = "ID";
+            public static final String COL_2 = "TERM";
+            public static final String COL_3 = "ANSWER";
+
+        }
+
+
+
+
+}
