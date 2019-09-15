67191196da88f6e222986c10bf949dbd632425b1
hiarpoetry <hiar.soul@gmail.com>
2019-09-15 11:48:17 +0530

    6th commit

--- app/src/main/java/com/example/student/headache2/level01.java
+++ app/src/main/java/com/example/student/headache2/level01.java
@@ -1,7 +1,9 @@
 package com.example.student.headache2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class level01 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class level01 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level01);
+
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this , Level2_Activity.class);
+        startActivity(intent);
+
+    }
+
 }
