1fd28689b7789f03396bbafd0b56039103d8e6ea
hiarpoetry <53651177+hiarpoetry@users.noreply.github.com>
2019-09-23 04:01:08 +0530

    Update lvl3_Activity.java
    commenting completed

--- app/src/main/java/com/example/student/headache2/lvl3_Activity.java
+++ app/src/main/java/com/example/student/headache2/lvl3_Activity.java
@@ -17,8 +17,8 @@ import Database.Level2Handler;
 import Database.Level3Handler;
 
 public class lvl3_Activity extends AppCompatActivity {
-
-    Level3Handler db = new Level3Handler(this);
+    
+    Level3Handler db = new Level3Handler(this); //create database handling object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,65 +27,65 @@ public class lvl3_Activity extends AppCompatActivity {
 
     }
 
-    public void sendMessage(View view){
+    public void sendMessage(View view){ //method for submit button
 
-        Intent i = getIntent();
-        final String username = i.getStringExtra("UserName");
+        Intent i = getIntent(); //create intent object
+        final String username = i.getStringExtra("UserName"); //get player username
 
-        Log.w("lola" , username);
+        Log.w("lola" , username); //log to check 
 
-        //assign values
+        //assign plain text/edit text values to string to read
         EditText a1 = findViewById(R.id.ans1);
-        String answer1 = a1.getText().toString();
+        String answer1 = a1.getText().toString(); //1st answer
 
         EditText a2 = findViewById(R.id.ans2);
-        String answer2 = a2.getText().toString();
+        String answer2 = a2.getText().toString(); //2nd answer
 
         EditText a3 = findViewById(R.id.ans3);
-        String answer3 = a3.getText().toString();
+        String answer3 = a3.getText().toString(); //3rd answer
 
-        //validation
+        //validation - if else 
         if (TextUtils.isEmpty(answer1) || TextUtils.isEmpty(answer2) || TextUtils.isEmpty(answer3)){
-            Toast warn = Toast.makeText(getApplicationContext(), "Enter Answers To Advance", Toast.LENGTH_LONG);
-            warn.show();
-        } //empty fields
+            Toast warn = Toast.makeText(getApplicationContext(), "Enter Answers To Advance", Toast.LENGTH_LONG); //create toast message
+            warn.show(); //display message
+        } //check for empty fields
 
-        else if (checkAnswer(answer1) && checkAnswer(answer2) && checkAnswer(answer3)){
-            Toast congrat = Toast.makeText(getApplicationContext(),"All Answers Are Successful! You May Advance To The Next Level!", Toast.LENGTH_LONG);
-            congrat.show();
+        else if (checkAnswer(answer1) && checkAnswer(answer2) && checkAnswer(answer3)){ //compare answers with read values
+            Toast congrat = Toast.makeText(getApplicationContext(),"All Answers Are Successful! You May Advance To The Next Level!", Toast.LENGTH_LONG); //create toast
+            congrat.show(); //display message
             //redirect to the next level
-            Intent intent = new Intent(this , Level04.class);
-            intent.putExtra("UserName", username);
+            Intent intent = new Intent(this , Level04.class); //ctreate intent 
+            intent.putExtra("UserName", username); //assign username to intent
             startActivity(intent);
 
-        } //successful answers
+        } //check for successful answers
 
         else {
-            Toast fail = Toast.makeText(getApplicationContext(),"Incorrect Answers. Please Try Again To Advance To The Next Level.", Toast.LENGTH_LONG);
-            fail.show();
+            Toast fail = Toast.makeText(getApplicationContext(),"Incorrect Answers. Please Try Again To Advance To The Next Level.", Toast.LENGTH_LONG); //create toast
+            fail.show();//display toast message
 
             Intent intent = new Intent(this, lvl3_Activity.class); //redirect to the same level
-            startActivity(intent);
+            startActivity(intent); 
 
-        } //incorrect answers
+        } //check for incorrect answers
 
     }
 
     //method to check answer
     public boolean checkAnswer(String answer){
-
-        List<String> list =  db.readAnswers();
-
+        //list to read answers
+        List<String> list =  db.readAnswers(); //readAnswer method used to fetch table data
+        //for loop
         for (String a : list){
-            if(answer.equalsIgnoreCase(a))//compare
-                return true;
+            if(answer.equalsIgnoreCase(a))//compare answers with table data to be equal
+                return true; 
         } //end of for loop
 
-        return false;
+        return false; //return false if not found
     }//end of check answer method
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed() { //prevent return button
 
     }
 
